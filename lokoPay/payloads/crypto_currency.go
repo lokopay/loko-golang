78a7fe090d82768dec2c64c389fbdc7e19340901
Bruce Chen <brchen@ashleyfurniture.com>
2025-05-27 17:22:17 +0800

    add chain id

--- lokoPay/payloads/crypto_currency.go
+++ lokoPay/payloads/crypto_currency.go
@@ -6,6 +6,7 @@ type CryptoCurrency struct {
 	Description string  `json:"description,omitempty"`
 	ID          string  `json:"id,omitempty"`
 	Network     string  `json:"network,omitempty"`
+	ChainId     string  `json:"chain_id,omitempty"`
 	Price       float64 `json:"price,omitempty"`
 	PricePair   string  `json:"price_pair,omitempty"`
 }
